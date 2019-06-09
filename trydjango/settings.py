0262fc828c76a38460ba687c785886dcf28a2847
Marcos <marcos2687@gmail.com>
2019-06-08 23:08:20 -0400

    Update settings.py

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trydjango',
+        'USER': 'postgres',
+        'PASSWORD': 'a',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
