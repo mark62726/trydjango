b11b0bd74f40cec584e5c3a8997900019067806b
Marcos <marcos2687@gmail.com>
2019-06-09 22:39:49 -0400

    Update urls.py

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view, contact_view, about_view, social_view, taderus_view
+from pages.views import home_view, contact_view, about_view, social_view
 urlpatterns = [
 	path('', home_view, name='home'),
     path('admin/', admin.site.urls),
